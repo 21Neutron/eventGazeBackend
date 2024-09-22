c5670167c64c9ae51a12a7310011685b7d3eeb32
21Neutron <mayankbisht2113@gmail.com>
2024-09-23 02:23:25 +0530

    first commit

---
+++ src/main/java/com/group9/eventgaze/EventgazeApplication.java
@@ -0,0 +1,13 @@
+package com.group9.eventgaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventgazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventgazeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group9/eventgaze/EventgazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group9.eventgaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventgazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
